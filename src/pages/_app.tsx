4b6bd7669061f60a29182bffdafc5cf19c7471b3
masaya0521 <kaji0521km@gmail.com>
2022-08-10 09:19:14 +0900

    api追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import styled, { createGlobalStyle } from "styled-components";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { createGlobalStyle } from 'styled-components';
 
 // グローバルのスタイル
 const GlobalStyle = createGlobalStyle`
